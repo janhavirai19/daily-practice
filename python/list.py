ef632f5de6f40e411c9565645b44423b312a9aef
janhavirai19 <janhavirai932@gmail.com>
2026-03-01 22:24:03 +0530

    -LIST

--- python/list.py
+++ python/list.py
@@ -1,7 +1,17 @@
 ## Reverse a List >>>>>>>>
 
-nums = [10,20,30,40,50]
-reversed_list=nums[:: -1]
+# nums = [10,20,30,40,50]
+# reversed_list=nums[:: -1]
 
-print("Original:",nums)
-print("Reversed:",reversed_list)
+# print("Original:",nums)
+# print("Reversed:",reversed_list)
+
+
+
+  
+# Remove Duplicates from List    >>>>>>>>>>>>>>>
+
+
+nums = [1,2,3,4,5,67,3,4]
+unique = list(set(nums))
+print(unique)
