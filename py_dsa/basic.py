201ef381ef272112bedb74f00352ea0dfc540ce6
janhavirai19 <janhavirai932@gmail.com>
2026-03-04 22:37:15 +0530

    dsa with py

--- py_dsa/basic.py
+++ py_dsa/basic.py
@@ -1,7 +1,30 @@
-arr=[12,78,87,56,7]
-maximum= arr[0]
+# FIND MAXIMUM NO. >>>>>>
+
+# arr=[12,78,87,56,7]
+# maximum= arr[0]
+
+# for num in arr:
+#     if num > maximum:
+#         maximum = num
+# print("maximum no. : ",maximum)
+
+
+
+ #EVEN & ODD FIND  >>>>>>>>>>>
+
+arr=[1,2,3,4,5,6,7]
+
+even=0
+odd=0
 
 for num in arr:
-    if num > maximum:
-        maximum = num
-print("maximum no. : ",maximum)
+    if num % 2 ==0:
+        even += 1
+    else:
+        odd+=1
+print("EVEN No. :",even)
+print("ODD No. :",odd)
+
+
+
+
