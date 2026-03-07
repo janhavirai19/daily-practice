b85a763564a7394908f4e80f30217e82372bf8fb
janhavirai19 <janhavirai932@gmail.com>
2026-03-08 01:15:44 +0530

    dsa with py

--- py_dsa/Contactsearch.py
+++ py_dsa/Contactsearch.py
@@ -9,6 +9,8 @@
 # else:
 #     print("Contact not found")
 
+
+
 # Output>>>>>>
 
     #  right input *********
@@ -18,3 +20,25 @@
     #  Wrong input   *******
 # Enter name: janhavi
 # Contact not found
+
+
+contacts = {
+    "Aman": 98765,
+    "Riya": 78544,
+    "Siya": 89643
+}
+name = input("Enter name: ").capitalize()
+if name in contacts:
+    print("📞", contacts[name])
+else:
+    print(" Not found")
+    for c in contacts:
+        if name[0] == c[0]:
+            print("🔎 Maybe:", c, "-", contacts[c])
+if input("Add new contact? (y/n): ").lower() == "y":
+    n = input("Name: ").capitalize()
+    num = int(input("Number: "))
+    contacts[n] = num
+print("\n📋 Contacts:")
+for k in sorted(contacts):
+    print(k, ":", contacts[k])
