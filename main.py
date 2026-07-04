b074a620ded7a1131e1ea454f572f5be188257f1
janhavirai19 <janhavirai932@gmail.com>
2026-07-05 02:23:19 +0530

    updated changes

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pandas as pd
 df = pd.read_csv("data/Students.csv")
 print(df.columns.tolist())
-# # print(df.head(3))
-# # print(df.tail(3))
+# print(df.head(3))
+# print(df.tail(3))
 # print(df["Name"])
 # print(df["Python"])
 df["Total"] = df["Python"] + df["Java"] + df["DSA"]
+# df["Percentage"] =df["Total"]/3
+print(df.loc[df["Total"].idxmax()])
 print(df)
-
 
 # # head, columns
