2a603549df8ca60c5e980041bf29ec47aeb15eb9
janhavirai19 <janhavirai932@gmail.com>
2026-07-05 02:17:51 +0530

    updated changes

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pandas as pd
 df = pd.read_csv("data/Students.csv")
+print(df.columns.tolist())
 # # print(df.head(3))
 # # print(df.tail(3))
-# # print(df["name"])
+# print(df["Name"])
 # print(df["Python"])
+df["Total"] = df["Python"] + df["Java"] + df["DSA"]
 print(df)
+
 
 # # head, columns
