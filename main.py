0c01a5b9455cc8e7a65a02bc2a95ef41f58a69a4
janhavirai19 <janhavirai932@gmail.com>
2026-07-05 02:04:01 +0530

    updated changes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
-df = pd.read_csv("data/students.csv")
-# print(df.head(3))
-print(df.tail(3))
+df = pd.read_csv("data/Students.csv")
+# # print(df.head(3))
+# # print(df.tail(3))
+# # print(df["name"])
+# print(df["Python"])
+print(df)
 
-# head, columns
+# # head, columns
