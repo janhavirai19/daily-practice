3183682c03c11d1c2fd1a0ba6c89cc1083d10f2e
janhavirai19 <janhavirai932@gmail.com>
2026-07-07 02:09:50 +0530

    updated changes

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pandas as pd
 df = pd.read_csv("data/Students.csv")
 print(df.columns.tolist())
-print(df.head(3))
-print(df.tail(3))
-print(df["Name"])
-print(df["Python"])
+# print(df.head(3))
+# print(df.tail(3))
+# print(df["Name"])
+# print(df["Python"])
 df["Total"] = df["Python"] + df["Java"] + df["DSA"]
-df["Percentage"] =df["Total"]/3
-print(df.loc[df["Total"].idxmax()])
-print(df[["Python","Java","DSA"]].mean())
+# df["Percentage"] =df["Total"]/3
+# print(df.loc[df["Total"].idxmax()])
+# print(df[["Python","Java","DSA"]].mean())
+# print(df)
+print(df.loc[df["Total"].idxmin()])
 print(df)
 
